7c919a7d6ba7105980dcbb6e42e48a3483d2f775
Jinshidjinu <jinshidjinu339@gmail.com>
2024-08-02 00:07:43 +0530

    admin side login page UI  completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter } from 'react-router-dom';
 import UserRouter from './Router/userRouter';
 import TeacherRouter from './Router/TeacherRouter';
 import AdminRouter from './Router/AdminRouter';
+
 const  App = () => {
   return (
    <div>
-    
       <BrowserRouter>
         <UserRouter/>
         <TeacherRouter/>
